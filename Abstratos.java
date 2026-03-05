d747634e480337c1bb3191a41c74bc75f000886b
Marcio-Salles <sallesmarcio1978@gmail.com>
2026-03-05 20:01:31 -0300

    Classe abstrata e polimorfismo

---
+++ Abstratos.java
@@ -0,0 +1,31 @@
+ abstract class FiguraGeometrica {
+ abstract double calcularArea();
+ abstract double calcularPerimetro();
+}
+
+class Retangulo extends FiguraGeometrica {
+	private double base;
+	private double altura;
+	public Retangulo(double base, double altura) {
+	this.base = base;
+	this.altura = altura;
+}
+@Override
+public double calcularArea() {
+	return base * altura;
+}
+@Override
+public double calcularPerimetro() {
+	return 2 * (base + altura);
+}
+}
+public class Abstratos {
+
+public static void main(String[] args) {
+	Retangulo retangulo = new Retangulo(5.0, 3.0);
+	System.out.println("Área do retângulo: " +
+	retangulo.calcularArea());
+	System.out.println("Perímetro do retângulo: " +
+	retangulo.calcularPerimetro());
+	}
+}
